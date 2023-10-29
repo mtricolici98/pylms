a68f16cee6251ab37c2dc2b51db222e11131c42e
antonio-marius.trico <antonio-marius.tricolici@prolion.com>
2023-10-29 21:43:24 +0100

    Revert "Initial Commit"
    This reverts commit 9566abe05480d866330d4cc2ad06a75aab23a5e4.

--- backend/pythonlms/users/tests/util.py
+++
@@ -1,9 +0,0 @@
-import string
-import random
-
-
-def generate_random_string() -> str:
-    result = "".join(
-        random.choices(string.ascii_lowercase + string.digits, k=8)
-    )
-    return result
